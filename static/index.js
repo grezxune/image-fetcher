28fd35182dfafb000a9dcd39985265bc4ffdd36f
tommy <tomtrezb2003@gmail.com>
2017-08-17 18:35:20 -0500

    focus back to url box after search returns

--- static/index.js
+++ static/index.js
@@ -21,6 +21,7 @@ ajax = {
             success: function (response) {
                 viewModel.imgUrls(response);
                 viewModel.isBusy(false);
+                $('#url').focus();
             },
             error: function (response) {
                 alert('Something went wrong, please try again');
